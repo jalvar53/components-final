2cc39aefea46c8ec9ded632a6d22dd3f9c442dd0
Jose Luis Alvarez <jalvarezh@psl.com.co>
2018-10-29 15:50:50 -0500

    First file structure

---
+++ src/main/java/co/edu/eafit/componentes/bankagent/BankagentApplication.java
@@ -0,0 +1,12 @@
+package co.edu.eafit.componentes.bankagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankagentApplication.class, args);
+	}
+}
---
+++ src/test/java/co/edu/eafit/componentes/bankagent/BankagentApplicationTests.java
@@ -0,0 +1,16 @@
+package co.edu.eafit.componentes.bankagent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankagentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
